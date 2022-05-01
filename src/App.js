5ebffb9f3770c7bd10a170cfac02af3430fb5653
fomula91 <fomula91@gmail.com>
2022-05-02 02:24:56 +0900

    김형중 feat : 와이어프레임  css 작업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Create from "./page/particreate";
+import View from "./page/partiview";
+import Coution from "./page/caution";
+import Mypage from "./page/mypage";
+import Main from "./page/main";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world josph!</h1>
+      <BrowserRouter>
+        <Route path="/" exact component={Main}></Route>
+        <Route path="/create" component={Create}></Route>
+        <Route path="/view" component={View}></Route>
+        <Route path="/coution" component={Coution}></Route>
+        <Route path="/mypage" component={Mypage}></Route>
+      </BrowserRouter>
     </div>
   );
 }
