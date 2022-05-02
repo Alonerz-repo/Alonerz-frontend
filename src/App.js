ee4a9531242cecd4a7591b4b39168f13afd48621
fomula91 <fomula91@gmail.com>
2022-05-02 20:41:36 +0900

    김형중 feat : 기능 추가 atom 디자인

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import View from "./page/partiview";
 import Coution from "./page/caution";
 import Mypage from "./page/mypage";
 import Main from "./page/main";
-import { Input } from "./element/index";
+import TestPage from "./page/TestPage";
+
+import { Input, Button, Text, Grid } from "./element/index";
 
 function App() {
   return (
@@ -17,7 +19,8 @@ function App() {
         <Route path="/view" component={View}></Route>
         <Route path="/coution" component={Coution}></Route>
         <Route path="/mypage" component={Mypage}></Route>
-        <Route path="/test" component={Input}></Route>
+        <Route path="/test" component={Button}></Route>
+        <Route path="/testpage" component={TestPage}></Route>
       </BrowserRouter>
     </div>
   );
