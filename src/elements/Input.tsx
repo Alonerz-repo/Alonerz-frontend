778942bf8914807d42870936e9eef94ce6456868
fomula91 <fomula91@gmail.com>
2022-05-06 17:15:11 +0900

    김형중 edit :  input, Text -> 패딩, 마진 파라미터추가

--- src/elements/Input.tsx
+++ src/elements/Input.tsx
@@ -2,37 +2,37 @@ import React, { forwardRef } from "react";
 import styled from "styled-components";
 import { Grid, Text } from "./index";
 
-type Props = {
-    text?: string;
-    placeholder?: string;
-    width?: string;
-    ref?: any;
-    bold?: boolean;
-    _onChange?: () => {};
-};
+interface Props {
+  text?: string;
+  placeholder?: string;
+  width?: string;
+  ref?: any;
+  bold?: boolean;
+  _onChange?: () => {};
+}
 
 const Input = forwardRef(
-    ({ text, placeholder, width, _onChange, ref, bold }: Props) => {
-        console.log(bold);
-        return (
-            <>
-                <Grid width={width}>
-                    <Text bold={bold}>{text}</Text>
-                    <ElementInput
-                        ref={ref}
-                        placeholder={placeholder}
-                        onChange={_onChange}
-                    />
-                </Grid>
-            </>
-        );
-    }
+  ({ text, placeholder, width, _onChange, ref, bold }: Props) => {
+    console.log(bold);
+    return (
+      <>
+        <Grid width={width}>
+          <Text bold={bold}>{text}</Text>
+          <ElementInput
+            ref={ref}
+            placeholder={placeholder}
+            onChange={_onChange}
+          />
+        </Grid>
+      </>
+    );
+  }
 );
 
 Input.defaultProps = {
-    text: "안녕하세요",
-    placeholder: "여기에 입력하세요",
-    width: "100%",
+  text: "안녕하세요",
+  placeholder: "여기에 입력하세요",
+  width: "100%",
 };
 
 const ElementInput = styled.input`
