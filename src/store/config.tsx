f073a3226923f1a102c6d87d02596509a49b2d1c
Kim_min_woo <alsdnsla44@naver.com>
2022-05-10 18:13:06 +0900

    김민우 edit: 수정

--- src/store/config.tsx
+++ src/store/config.tsx
@@ -4,14 +4,12 @@ import { createLogger } from "redux-logger";
 import partyInfoSlice from "./slices/partyInfoSlice";
 import userSlice from "./slices/userSlice";
 import getUserSlice from "./slices/getUserSlice";
-import imageSlice from "./slices/imageSlice";
 
 const logger = createLogger();
 const rootReducer = combineReducers({
   party: partyInfoSlice.reducer,
   user: userSlice.reducer,
   getUser: getUserSlice.reducer,
-  image: imageSlice.reducer,
 });
 
 const initialState = {};
