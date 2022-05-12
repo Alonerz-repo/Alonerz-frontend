4f044f322fad4619ed1bb265694b3a1186807b81
fomula91 <fomula91@gmail.com>
2022-05-12 16:20:01 +0900

    김형중 edit : card컴포넌트 클릭이벤트 및 이미지 url 삽입

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,6 +9,8 @@ type Props = {
   isFlex?: boolean;
   limit: Number;
   headcount: Number;
+  _onClick?(): void;
+  src?: string;
 };
 
 const Card = ({
@@ -19,12 +21,14 @@ const Card = ({
   isFlex,
   limit,
   headcount,
+  _onClick,
+  src,
 }: Props) => {
   const able = limit === headcount;
   if (isFlex) {
     return (
       <React.Fragment>
-        <BackgroundImage isFlex>
+        <BackgroundImage isFlex onClick={_onClick} src={src}>
           <Abled able={able} />
           <PartyInfo isFlex>
             <div>{title}</div>
@@ -41,7 +45,7 @@ const Card = ({
 
   return (
     <React.Fragment>
-      <BackgroundImage>
+      <BackgroundImage onClick={_onClick}>
         <PartyInfo>
           <div>{title}</div>
           <div>{address1}</div>
@@ -54,6 +58,7 @@ const Card = ({
 
 interface BackgroundImageProps {
   isFlex?: boolean;
+  src?: string;
 }
 
 interface PartyHeadcountProps {
@@ -81,15 +86,19 @@ const Abled = styled.div<AbledProps>`
 
 const BackgroundImage = styled.div<BackgroundImageProps>`
   position: relative;
-  background: skyblue;
+
   border-radius: 15px;
   width: ${(props) => (props.isFlex ? "48%" : "100%")};
   height: ${(props) => (props.isFlex ? "222px" : "130px")};
-  background: ${(props) =>
+  background-image: ${(props) =>
+    props.src
+      ? `linear-gradient(180deg, rgba(0, 0, 0, 0) 48.96%, rgb(0, 0, 0) 100%), url("${props.src}");`
+      : `linear-gradient(0deg, rgba(110,110,110,1) 0%, rgba(255,255,255,0) 60%), url("${props.src}");`};
+  /* background: ${(props) =>
     props.isFlex
       ? "linear-gradient(180deg, rgba(0, 0, 0, 0) 48.96%, rgba(0, 0, 0, 0.35) 100%)"
-      : "linear-gradient(0deg, rgba(110,110,110,1) 0%, rgba(255,255,255,0) 60%)"};
-  background-color: skyblue;
+      : "linear-gradient(0deg, rgba(110,110,110,1) 0%, rgba(255,255,255,0) 60%)"}; */
+  color: white;
   margin-bottom: 10px;
 `;
 
@@ -103,7 +112,7 @@ const PartyHeadcount = styled.div<PartyHeadcountProps>`
   height: 28px;
   border-radius: 10px;
   padding: 3px 4px;
-  background: aliceblue;
+  background: #424242;
   right: 10px;
   top: 10px;
   text-align: center;
