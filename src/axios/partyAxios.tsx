440590091aa075a97128e82a16cab41e817d72ed
choewy <choewy32@gmail.com>
2022-05-14 03:40:34 +0900

    최원영 refactor : axios 부분 refactorying

--- src/axios/partyAxios.tsx
+++ src/axios/partyAxios.tsx
@@ -1,11 +1,12 @@
-import axios from "axios";
-import { getCookie } from "../utils/cookie";
+import axios from 'axios';
+import { errorHandler, getHeaders, getUrl } from '../utils/api';
 
 interface userInfo {
   userId: number;
   nickname: string;
   profileImageUrl: string;
 }
+
 export interface GroupInfo {
   groupId?: number;
   title: string;
@@ -25,97 +26,78 @@ export interface GroupInfo {
   guests: userInfo[];
 }
 
-const token = getCookie("accessToken");
-const url = `http://localhost:5000`;
+// 최원영
+export type Group = Partial<GroupInfo>;
 
 export const partyAxios = {
-  createParty: async (group: Partial<GroupInfo>) => {
-    try {
-      await axios({
-        method: "post",
-        url: `${url}/api/groups`,
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-        data: {
-          ...group,
-        },
-      }).then((res) => {
-        console.log(res.status);
-        console.log(res.data);
-      });
-    } catch (err: any) {
-      console.log(err);
-    }
+  // 최원영
+  createParty: async (group: Group) => {
+    const url = getUrl(`/api/groups`);
+    const headers = getHeaders();
+    const body = { ...group };
+    const data = await axios
+      .post(url, body, { headers })
+      .then((response) => response.data)
+      .catch((error) => error.response.data);
+
+    return data.error ? errorHandler(data) : data;
   },
 
-  editParty: async (group: Partial<GroupInfo>, groupId: number) => {
-    try {
-      await axios({
-        method: "patch",
-        url: `${url}/api/groups${groupId}`,
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-        data: {
-          ...group,
-        },
-      }).then((res) => {
-        console.log(res.status);
-        console.log(res.data);
-      });
-    } catch (err: any) {
-      console.log(err);
-    }
+  // 최원영
+  editParty: async (group: Group, groupId: number) => {
+    const url = getUrl(`/api/groups/${groupId}`);
+    const headers = getHeaders();
+    const body = { ...group };
+    const data = await axios
+      .patch(url, body, { headers })
+      .then((response) => response.data)
+      .catch((error) => error.response.data);
+
+    return data.error ? errorHandler(data) : data;
   },
 
+  // 최원영
   getPartyInfo: async (groupId: number) => {
-    try {
-      const result = await axios({
-        method: "get",
-        url: `${url}/api/groups/${groupId}`,
-        data: {
-          userId: 1,
-          groupId,
-        },
-      });
-      console.log(result.data.group);
-      console.log(new Date(Date.parse(result.data.group.endAt)));
-      return result.data.group;
-    } catch (err: any) {
-      console.log(err.message);
-    }
+    const url = getUrl(`/api/groups/${groupId}`);
+    const headers = getHeaders();
+    const data = await axios
+      .get(url, { headers })
+      .then((response) => response.data)
+      .catch((error) => error.response.data);
+
+    return data.error ? errorHandler(data) : data.group;
   },
 
   initialState: {
     group: {
       groupId: -1,
-      title: "",
-      menu: "",
-      description: "",
+      title: '',
+      menu: '',
+      description: '',
       startAt: new Date(),
       endAt: new Date(),
       limit: 4,
-      imageUrl: "",
+      imageUrl: '',
       locationX: 33.450701,
       locationY: 126.570667,
-      address: "",
-      placeName: "",
-      createdAt: "",
-      updateAt: "",
+      address: '',
+      placeName: '',
+      createdAt: '',
+      updateAt: '',
       host: {
         userId: 0,
-        nickname: "",
-        profileImageUrl: "",
+        nickname: '',
+        profileImageUrl: '',
       },
       guests: [
         {
           userId: 0,
-          nickname: "",
-          profileImageUrl: "",
+          nickname: '',
+          profileImageUrl: '',
         },
       ],
     },
   },
 };
+
 export default partyAxios;
