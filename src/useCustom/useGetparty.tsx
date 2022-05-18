1b571b9b6d5ec06a13b06c7a345c318dfc7b91a3
Kim_min_woo <alsdnsla44@naver.com>
2022-05-18 17:01:42 +0900

    김민우 feat: 중요 페이지 주석

--- src/useCustom/useGetparty.tsx
+++ src/useCustom/useGetparty.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import partyAxios, { GroupInfo, initialState } from "../axios/partyAxios";
 
+// 파티 상세정보를 받아와 return해주는 훅
 const useGetparty = (groupId: string | undefined) => {
   const [group, setGroup] = useState<GroupInfo>(initialState);
   useEffect(() => {
