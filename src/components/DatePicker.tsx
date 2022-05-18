472073075dfcf8e24b5cde822a41d798058e405b
Kim_min_woo <alsdnsla44@naver.com>
2022-05-19 02:31:07 +0900

    김민우 feat: 주석

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -4,10 +4,12 @@ import "react-datepicker/dist/react-datepicker.css";
 import ko from "date-fns/locale/ko";
 import "./DatePicker.css";
 
+// 달력 생성 컴포넌트
 const DatePickerComponent = () => {
   const [startDate, setStartDate] = useState(new Date());
 
   return (
+    // 한글 달력, 오늘 날짜부터 선택 가능
     <DatePicker
       selected={startDate}
       onChange={(date: any) => setStartDate(date)}
