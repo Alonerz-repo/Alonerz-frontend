f368895725787cf50edbdb7958e395231620b69e
fomula91 <fomula91@gmail.com>
2022-05-27 22:47:30 +0900

    김형중  feat: 로그인 에셋 추가

---
+++ src/assets/login/index.ts
@@ -0,0 +1,30 @@
+import data from "./data.json";
+const max = 3;
+const extension = ".svg";
+
+interface Row {
+  id: number;
+  image: string;
+  item: string;
+}
+
+class loginModule {
+  constructor(
+    public readonly rows: Row[] = [...Array(max)].map((_, id) => {
+      const imageName = ("0" + (id + 1)).slice(-2);
+      const image = require(`./${imageName}${extension}`);
+      const row: Row = {
+        id,
+        image: String(image),
+        item: String(data[id]),
+      };
+      return row;
+    })
+  ) {}
+
+  findById(id: number): Row | undefined {
+    return this.rows.find((row) => row.id === id);
+  }
+}
+
+export default new loginModule();
