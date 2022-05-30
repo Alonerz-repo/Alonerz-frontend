89579b6ba482a551b9d87a31b8aeaec93b4a5226
Kim_min_woo <alsdnsla44@naver.com>
2022-05-30 18:32:08 +0900

    김민우 fix: ios select

--- src/pages/EditGroup/Select.tsx
+++ src/pages/EditGroup/Select.tsx
@@ -25,29 +25,6 @@ interface Props {
   setValue: UseFormSetValue<CreateForm>;
 }
 
-const MyOption = styled.option`
-  background: #eeeeee;
-  text-align: unset;
-  color: black;
-`;
-
-const MySelected = styled.select<MySelectedProps>`
-  /* text-align: center; */
-  background: #eeeeee;
-  border-radius: 20px;
-  border: none;
-  outline: 0px;
-  width: ${(props) => props.width ?? "100%"};
-  margin: ${(props) => props.margin ?? ""};
-  height: 44px;
-  padding: 0px 20px;
-  /* background: url("data:image/svg+xml,%3Csvg width='14' height='14' viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='7' cy='7' r='7' fill='white'/%3E%3C/svg%3E%0A")
-    no-repeat 95% 50%; */
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-`;
-
 // 컴포넌트
 const Select = ({ width, margin, register, setValue, getValues }: Props) => {
   const [start, setStart] = React.useState<number>();
@@ -106,5 +83,23 @@ interface MySelectedProps {
   width?: string;
   margin?: string;
 }
+
+const MyOption = styled.option`
+  background: #eeeeee;
+  text-align: unset;
+  color: black;
+`;
+
+const MySelected = styled.select<MySelectedProps>`
+  /* text-align: center; */
+  background: #eeeeee;
+  border-radius: 20px;
+  border: none;
+  outline: 0px;
+  width: ${(props) => props.width ?? "100%"};
+  margin: ${(props) => props.margin ?? ""};
+  height: 44px;
+  padding: 0px 20px;
+`;
 
 export default Select;
